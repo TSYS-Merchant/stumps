2b054f01e69caadff00d064ad5ec0d03125b3c2a
Michael Harrington <mharrington@merchantwarehouse.com>
2013-12-31 07:26:14 -0500

    Added JSON serialization tests.

---
+++ tests/Stumps.Tests/SampleJsonObject.cs
@@ -0,0 +1,30 @@
+﻿namespace Stumps {
+
+    using System;
+
+    public class SampleJsonObject {
+
+        public enum SampleValues {
+            Value1,
+            Value2,
+            Value3
+        }
+
+        public string Color {
+            get;
+            set;
+        }
+
+        public int Number {
+            get;
+            set;
+        }
+
+        public SampleValues EnumerationValue {
+            get;
+            set;
+        }
+
+    }
+
+}
