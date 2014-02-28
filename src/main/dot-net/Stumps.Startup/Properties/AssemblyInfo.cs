102a31ca037786eedbf219edf2fcbfac8de15d98
Michael Harrington <mharrington@merchantwarehouse.com>
2014-02-28 14:36:46 -0500

    Split the AssemblyInfo.cs classes into multiple assembly files.

--- src/main/dot-net/Stumps.Startup/Properties/AssemblyInfo.cs
+++ src/main/dot-net/Stumps.Startup/Properties/AssemblyInfo.cs
@@ -9,31 +9,7 @@ using System.Resources;
 [assembly: AssemblyTitle("Stumps.Startup")]
 [assembly: AssemblyDescription("Stumps Application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Merchant Warehouse")]
-[assembly: AssemblyProduct("Stumps")]
-[assembly: AssemblyCopyright("Copyright © 2013 Merchant Warehouse")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ae9efa41-4aaa-4568-9611-7a9b03d13c47")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en")]
-[assembly: CLSCompliant(false)]
--- src/test/unit/Stumps.Tests/Properties/AssemblyInfo.cs
+++ src/test/unit/Stumps.Tests/Properties/AssemblyInfo.cs
@@ -4,36 +4,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyTitle("Stumps.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Stumps Runtime Engine Tests")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Stumps.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("abc57f50-ac4d-4bd0-93ea-c0fcff54e7e6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("3a65988b-7beb-4d38-b944-4390b9ea8c7f")]
