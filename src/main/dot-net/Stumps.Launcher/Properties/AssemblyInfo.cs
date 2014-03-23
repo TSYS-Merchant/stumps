739b098295ee7363dad68349dba03ba8ad2023c3
Michael Harrington <mharrington@merchantwarehouse.com>
2014-03-23 08:55:21 -0400

    Added new application to launch the stumps dashboard.

---
+++ src/main/dot-net/Stumps.Launcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Stumps.Launch")]
+[assembly: AssemblyDescription("Stumps Dashboard Launcher")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ae9efa41-4aaa-4568-9611-7a9b03d13c47")]
