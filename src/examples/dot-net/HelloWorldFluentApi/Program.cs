d8bf157bc7e93380d1e4c80bc1b3b290cf7ac10f
Michael Harrington <mharrington@merchantwarehouse.com>
2014-03-25 11:56:24 -0400

    Added some basic examples for using the Stumps API.

---
+++ src/examples/dot-net/HelloWorldFluentApi/Program.cs
@@ -0,0 +1,61 @@
+namespace Stumps.Examples.HelloWorldFluentApi
+{
+
+    /* Stumps Hello World Fluent API Example
+     * =====================================
+     * 
+     * This Example is used to demonstrate using the Fluent API extensions provided 
+     * by Stumps to create a new instance of a Stumps HTTP server which will respond 
+     * back to the URL /HelloWorld.htm.
+     */
+
+    using System;
+    using Stumps;
+
+    /// <summary>
+    ///     A class that provides the primary entry point for the application.
+    /// </summary>
+    public class Program
+    {
+
+        /// <summary>
+        ///     Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+
+            ConsoleHelper.ApplicationBanner("Hello World Fluent API");
+
+            // Create a new Stumps Server
+            var server = new StumpsServer();
+
+            // Showing off some chaining: Make the server return an HTTP 404 for all unknown
+            // and then when the URL /HelloWorld.htm is requested, return back an HTML page
+            // that was loaded from a file.
+            server.RespondsWithHttp404()
+                  .HandlesRequest("HelloWorld").MatchingMethod("GET")
+                                               .MatchingUrl("/HelloWorld.htm")
+                                               .Responds().WithFile("HelloWorld.htm");
+            
+            // Show the requests that are incomming
+            server.RequestProcessed += (o, e) => ConsoleHelper.ShowHttpResponse(server, e);
+
+            // Start the server and wait!
+            server.Start();
+
+            // Show the URL to the user
+            Console.WriteLine("Browse to http://localhost:{0}/HelloWorld.htm", server.ListensOnPort);
+            Console.WriteLine();
+
+            // Wait to exit
+            ConsoleHelper.WaitForExit();
+
+            server.Shutdown();
+            server.Dispose();
+
+        }
+
+    }
+
+}
