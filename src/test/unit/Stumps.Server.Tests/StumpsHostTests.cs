1a7b3ac33928560c6667b8716bcdfa6fb8cba462
dlachowicz-mw <dlachowicz@merchantwarehouse.com>
2014-03-26 09:24:20 -0400

    StumpHostTests.Constructor_PortInUse_ThrowsException() works

--- src/test/unit/Stumps.Server.Tests/StumpsHostTests.cs
+++ src/test/unit/Stumps.Server.Tests/StumpsHostTests.cs
@@ -3,8 +3,10 @@
 
     using System;
     using System.Net;
+	using System.Net.Sockets;
     using NSubstitute;
     using NUnit.Framework;
+	using Stumps;
     using Stumps.Server.Data;
     using Stumps.Server.Logging;
 
@@ -34,10 +36,35 @@
         [Test]
         public void Constructor_PortInUse_ThrowsException()
         {
-            StumpsHost proxy = new StumpsHost(Substitute.For<IServerFactory>(), Substitute.For<ILogger>(), Substitute.For<IDataAccess>());
-            Assert.That(
-                () => proxy.CreateServerInstance("www.foo.com", 135, true, false),
-                Throws.Exception.TypeOf<StumpsNetworkException>().With.Property("Message").EqualTo("The port is already in use."));
+			int port = NetworkInformation.FindRandomOpenPort();
+			string externalHostName = "www.foo.com";
+			bool autoStart = false;
+			bool useSsl = true;
+
+			var proxyEntity = new ProxyServerEntity
+			{
+				AutoStart = autoStart,
+				ExternalHostName = externalHostName,
+				Port = port,
+				UseSsl = useSsl,
+				ProxyId = Stumps.Server.Utility.RandomGenerator.GenerateIdentifier()
+			};
+
+			var dataAccess = Substitute.For<IDataAccess>();
+			dataAccess.ProxyServerFind (Arg.Any<string>()).Returns(proxyEntity);
+
+			// create a TcpListener already listening on the port
+			var tcpListener = new TcpListener (port);
+			try {
+				tcpListener.Start ();
+
+				StumpsHost proxy = new StumpsHost(Substitute.For<IServerFactory>(), Substitute.For<ILogger>(), dataAccess);
+				Assert.That(
+					() => proxy.CreateServerInstance(externalHostName, port, useSsl, autoStart),
+	                Throws.Exception.TypeOf<StumpsNetworkException>().With.Property("Message").EqualTo("The port is already in use."));
+			} finally {
+				tcpListener.Stop ();
+			}
         }
 
         [Test]
