2872fa646e2637b0675034e71bb8dd43935fbab2
Michael Harrington <mharrington@cayan.com>
2018-07-10 17:22:16 -0400

    Release Activity: Bumped to version v0.4.0
    Bumped the version number to 0.4 and updated the copyright information.

--- src/main/dot-net/CommonAssembly/ProductAssemblyInfo.cs
+++ src/main/dot-net/CommonAssembly/ProductAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Cayan, LLC")]
 [assembly: AssemblyProduct("Stumps")]
-[assembly: AssemblyCopyright("Copyright \x00a9 2017 Cayan LLC All Rights Reserved.")]
+[assembly: AssemblyCopyright("Copyright \x00a9 2018 Cayan LLC All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
